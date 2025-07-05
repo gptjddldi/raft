7231d987e9a194c460adaa5f03b19cba45acb564
gptjddldi <hystom38@gmail.com>
2025-07-05 14:46:47 +0900

    initial structure

--- raft.go
+++ raft.go
@@ -1,8 +1,32 @@
 package mai
 
-type Raft struct {
+type ConsensusModule struct {
 	id      int
 	peerIds []int
 
 	// server *Server
+}
+
+func (cm *ConsensusModule) runElectionTimer() {
+
+}
+
+func (cm *ConsensusModule) startElection() {
+
+}
+
+func (cm *ConsensusModule) startLeader() {
+
+}
+
+func (cm *ConsensusModule) leaderSendHeartbeats() {
+
+}
+
+func (cm *ConsensusModule) RequestVote() {
+
+}
+
+func (cm *ConsensusModule) AppendEntries() {
+
 }
